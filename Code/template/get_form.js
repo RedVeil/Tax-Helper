96daa2c324d6ece49cbfa8e05fbca09a2a2c6e7d
Leon Niesler <leon.niesler@gmail.com>
2019-04-12 08:17:09 +0200

    dict push

--- Code/template/get_form.js
+++ Code/template/get_form.js
@@ -1,40 +1,51 @@
 document.addEventListener("DOMContentLoaded",function() {
 
+    let counter = 0
+    var dictInputs= {"name": "", "vorname":"", "strasse":""}
+    
+    const start = document.querySelector(".start");
+    start.addEventListener('click', createParagraph);
+    
+    
+    
+    function createParagraph() {
+        if (counter === 0){
+            start.parentNode.removeChild(start)
+        }
+        var form = document.createElement('form');
+        var input = document.createElement("INPUT");
+        var submit = document.createElement("button");
+        input.setAttribute("type", "text");
+        input.setAttribute("id", "input");
+        input.setAttribute("placeholder", "Name");
+        submit.setAttribute("class", "button");
+        submit.setAttribute("value", "submit");
+        submit.addEventListener('click', addToDict);
+        form.appendChild(input);
+        form.appendChild(submit);
+        document.body.appendChild(form);
+    };
 
-var dictInputs = {"name":"test"};
-
-const start = document.querySelector(".start");
-start.addEventListener('click', createParagraph);
-
-const submit = document.querySelector(".submit");
-submit.addEventListener('click', addToDict);
-
-
-
-function test(){
-    var iban = document.getElementById("iban")
-    var message = iban.value;
-    var paragraphEl = document.createElement('p');
-    paragraphEl.textContent = iban.value;
-    document.body.appendChild(paragraphEl);
-};
-
-
-function addToDict(){
-    var iban = document.getElementById("iban")
-    dictInputs.push({
-        key:   "iban",
-        value: iban.value,
+    function addToDict(){
+        let input_key = Object.keys(dictInputs)[counter];
+        console.log(input_key)
+        var input = document.getElementById("input");  
+        dictInputs[input_key] = input.value;
+        var paragraphEl = document.createElement('p');
+        paragraphEl.textContent = dictInputs[input_key];
+        document.body.appendChild(paragraphEl);
+        counter += 1
+        createParagraph();
+        //console.log(input_key)
+    };
     });
-    var paragraphEl = document.createElement('p');
-    paragraphEl.textContent = dictInputs["iban"];
-    document.body.appendChild(paragraphEl);
-};
-
-
-function createParagraph(){
-    var paragraphEl = document.createElement('p');
-    paragraphEl.textContent = dictInputs["name"];
-    document.body.appendChild(paragraphEl);
-};
-});
+    
+    
+    /*
+    1. move through list of inputs_names with submit of createParagraph
+    2. Safe input as variable
+    3. submit variables to backend
+    4. create back button
+    5. create cards and visuable feedback
+    6. create info button
+    7. create progress bar*/
--- Code/template/script.js
+++ Code/template/script.js
@@ -1,33 +1,56 @@
 document.addEventListener("DOMContentLoaded",function() {
 
-
-var inputDict= {"name": "", "vorname":"", "strasse":""}
+let counter = 0
+var dictInputs= {"name": "", "vorname":"", "strasse":""}
 
 const start = document.querySelector(".start");
-start.addEventListener('click', createParagraph);
+start.addEventListener('click', createParagraph,);
 
 
 
 function createParagraph() {
-    start.parentNode.removeChild(start)
-    var form = document.createElement('form');
+    let input_key = Object.keys(dictInputs)[counter];
+    if (counter === 0){
+        start.parentNode.removeChild(start);
+    }
+    var card = document.createElement('div');
     var input = document.createElement("INPUT");
-    var submit = document.createElement("INPUT")
+    var submit = document.createElement("button");
     input.setAttribute("type", "text");
-    input.setAttribute("placeholder", "Name");
-    submit.setAttribute("type", "submit");
-    form.appendChild(input);
-    form.appendChild(submit);
-    form.setAttribute("id", "id_1");
-    document.getElementById("formSpace").appendChild(form);
+    input.setAttribute("id", "input");
+    input.setAttribute("placeholder", input_key);
+    submit.setAttribute("value", "test")
+    submit.setAttribute("class", "button")
+    submit.addEventListener('click', addToDict);
+    card.appendChild(input);
+    card.appendChild(submit);
+    card.setAttribute("id", input_key)
+    document.body.appendChild(card);
+};
+
+/*function createButton(){
     const submitAll = document.createElement("button");
     submitAll.setAttribute("name", "helloButton");
+    submitAll.setAttribute("class", "button")
     submitAll.addEventListener('click', safeToDb);
-    document.getElementById("submitAllSpace").appendChild(submitAll);
-}
+    document.body.appendChild(submitAll);
+};
 
 function safeToDb() {
-}
+};
+*/
+
+function addToDict(){
+    let input_key = Object.keys(dictInputs)[counter];
+    var input = document.getElementById("input");  
+    dictInputs[input_key] = input.value;
+    var paragraphEl = document.createElement('p');
+    paragraphEl.textContent = dictInputs[input_key];
+    document.body.appendChild(paragraphEl);
+    //destroy or hide previous card
+    counter += 1
+    createParagraph();
+};
 });
 
 
@@ -38,4 +61,5 @@ function safeToDb() {
 4. create back button
 5. create cards and visuable feedback
 6. create info button
-7. create progress bar*/
+7. create progress bar
+*/
