a19ce2fa22291cfbe5ccffdbfdeed9e38df28681
Leon Niesler <leon.niesler@gmail.com>
2019-04-24 12:37:55 +0200

    init test

--- Code/__init__.py
+++ Code/__init__.py
@@ -2,18 +2,16 @@ import os
 from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, SubmitField
 from flask import Flask, render_template, redirect, request
+from . import mainpage
 
-def create_app():
-    # create and configure the app
-    app = Flask(__name__,)
-    app.config['SECRET_KEY'] = 'test'
+app = Flask(__name__,)
+app.config['SECRET_KEY'] = 'test'
 
 
-    @app.route('/', methods=("GET","POST"))    
-    def hello():
-        return render_template("index.html")
+@app.route('/', methods=("GET","POST"))    
+def hello():
+    return render_template("index.html")
 
-    from . import mainpage
-    app.register_blueprint(mainpage.bp)
+app.register_blueprint(mainpage.bp)
 
-    return app
+app.run("0.0.0.0", port=80)
